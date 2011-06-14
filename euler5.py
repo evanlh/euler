b3f3ee05a3090f50d1706ff034ddc3fa307287e8
Evan Lawrence-Hurt <evanlh@gmail.com>
2011-06-14 09:59:45 -0400

    Finished euler5.py

--- euler5.py
+++ euler5.py
@@ -2,8 +2,32 @@
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"""
 
-def solve():
-	return 0
+def primes_less_than(n):
+	""" Returns a list of all the prime numbers less than n. Brute-force, don't use for large n."""
+	return [ x for x in range(2,n + 1) if sum([y for y in range(2, x) if x % y == 0]) == 0]
 
 if __name__ == "__main__":
-	print solve()
+	""" my general approach: 1) find the maximum power of each prime that divides evenly into the dividers, thus avoiding a prime factor decomposition 3) multiply them together. """
+
+	dividers = range(2,21) # everything is divisible by 1 so byebye
+	primes = primes_less_than(20)
+	result = {}
+	for d in dividers:
+		for p in primes:
+			if p <= d:
+				i = 1
+				while d % (p**i) == 0:
+					if p in result:
+						if i > result[p]:
+							result[p] = i
+					else:
+						result[p] = i
+
+					i += 1
+	print result
+
+	i = 1
+	for k in result:
+		i *= k**result[k]
+
+	print i
