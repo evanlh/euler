13b1510c02a784d7cf1c138b86e039ab292e12fe
Evan Lawrence-Hurt <evanlh@gmail.com>
2011-06-14 15:43:02 -0400

    Finished solution to problem 10

---
+++ euler10.py
@@ -0,0 +1,22 @@
+"""Find the sum of all the primes below two million."""
+from math import sqrt
+def primes_up_to(n):
+	"""More efficient way of finding the primes below n"""
+	primes = [2]
+	i =	max(primes) + 1 
+	halt = sqrt(n)
+	while i <= n:
+		for p in primes:
+			if i % p == 0:
+				break
+			if p > halt:
+				primes.append(i)
+				break
+		else:
+			primes.append(i)
+		i+=1
+	return primes
+
+if __name__ == "__main__":
+	print sum(primes_up_to(2000000))
+
