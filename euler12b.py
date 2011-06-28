fea6b7d9e16256552273adad59be2ba56b36e7e0
Evan Lawrence-Hurt <evanlh@gmail.com>
2011-06-28 16:22:34 -0400

    More optimizations, down to < 5 secs

--- euler12b.py
+++ euler12b.py
@@ -1,12 +1,12 @@
-""" Slightly more efficient solution to problem 12: What is the value of the first triangle number to have over five hundred divisors?"""
+""" More efficient solution to problem 12: What is the value of the first triangle number to have over five hundred divisors?"""
 
 from math import sqrt, log
 from euler10 import primes_sieve, sieve_between, primes_up_to
 from operator import mul
 
-PRIMES  = primes_sieve(1000000) # get all primes < one million
 
 def nth_triangle (n):
+	""" Returns the nth triangle number."""
 	accum = n
 	while n > 0:
 		n -= 1
@@ -14,7 +14,21 @@ def nth_triangle (n):
 
 	return accum
 
+def triangles ():
+	""" Yields an (index, triangle) tuple of triangle numbers and their corresponding index. Ex usage: for (index, triangle) in triangles():"""
+	accum = 0
+	i = 1
+	while 1:
+		accum += i
+		yield i, accum
+		i += 1
+
+
 def num_divisors (n):
+	""" Calculates the number of divisors of n, based on the fact that for a prime decomposition of n into (p1**v1)(p2**v2)..(pn**vn) the number of divisors is equivalent to (v1 + 1)(v2 + 1)..(vn + 1) """
+	global PRIMES		# this is very poor form. best way to remove while maintaining performance? pass as argument?
+	if abs(n) <= 3:
+		return 1
 	halt = sqrt(n)
 	divs = dict()
 	for p in PRIMES:
@@ -30,11 +44,10 @@ def num_divisors (n):
 
 if __name__ == "__main__":
 
-	i, count = 7, 0
-	while count < 500:
-		triangle = nth_triangle (i)
-		count = num_divisors(triangle)
+	PRIMES  = primes_sieve(1000000) # get all primes < one million
+	for (index, t) in triangles():
+		count = num_divisors(t)
 		if count >= 500:
-			print i, triangle, count
-		i += 1
+			print index, t, count
+			break
 
